9fa094fe4b1e04b5324b2700c4e6fa816dc9cf73
zhu <710738830@qq.com>
2023-11-03 16:08:03 +0800

    first commit

---
+++ src/test/java/com/zhz/myspringcloud/MySpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhz.myspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
